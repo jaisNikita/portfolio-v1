6451387fcceef16b7778dd68f8264d6f074692be
jaisNikita <jaiswalnikita655@gmail.com>
2023-08-24 01:50:02 +0530

    test commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("script running...");
+document.querySelector('.cross').getElementsByClassName.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", () => {
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if (document.querySelector('.sidebar').classList.contains('sidebarGo')) {
+
+    }
+})
